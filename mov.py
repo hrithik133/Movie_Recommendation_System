8336bae410a37159222942618aafb006a0030ee5
Hrithik Shukla <122604143+hrithik133@users.noreply.github.com>
2023-05-28 14:46:02 -0400

    Updates_1

--- mov.py
+++ mov.py
@@ -2,8 +2,24 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import gdown
 
-# Load movie data and cosine similarity matrix
+# Define the file IDs of the .pkl files on Google Drive
+file_id_1 = '1iqO6x8K71y79mFAtSOlVftXEfsHt6qxH'
+file_id_2 = '1taSPG6lipC8pthNTj4weIRXb8H9eQJju'
+
+# Define the URLs to download the file from Google Drive
+url_1 = f'https://drive.google.com/uc?id={file_id_1}'
+url_2 = f'https://drive.google.com/uc?id={file_id_2}'
+
+# Download the file from Google Drive
+output_1 = 'movie_dict.pkl'
+output_2 = 'similarity.pkl'
+
+gdown.download(url_1, output_1, quiet=False)
+gdown.download(url_2, output_2, quiet=False)
+
+# Load the pickle file
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 cosine_sim = pickle.load(open('similarity.pkl', 'rb'))
